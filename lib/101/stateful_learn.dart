18f542d52712726a42e1a935cca2ce0cd37ab5cf
ahmetcakr <ahmetcaakirr@gmail.com>
2022-07-04 22:45:05 +0300

    updated stateful learn

--- lib/101/stateful_learn.dart
+++ lib/101/stateful_learn.dart
@@ -10,9 +10,57 @@ class StatefulLearn extends StatefulWidget {
 }
 
 class _StatefulLearnState extends State<StatefulLearn> {
+  int _countValue = 0;
+
+  void _updateCounter(bool isIncrement) {
+    if (isIncrement) {
+      _countValue += 1;
+    } else {
+      _countValue -= 1;
+    }
+    setState(() {});
+  }
+
   // değişiklikler burada, hayat burada gerçekleşir.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [_incrementButton(), _deincrementPadding()],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      body: Center(
+          child: Text(
+        _countValue.toString(),
+        style: Theme.of(context).textTheme.headline2,
+      )),
+    );
+  }
+
+  FloatingActionButton _incrementButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        _updateCounter(true);
+      },
+      child: const Icon(Icons.add),
+    );
+  }
+
+  Padding _deincrementPadding() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10),
+      child: _deincrementButton(),
+    );
+  }
+
+  FloatingActionButton _deincrementButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        _updateCounter(false);
+      },
+      child: const Icon(Icons.remove),
+    );
   }
 }
